154206018ff9e12a53cbe1c41657204b2dc10a6e
Munggok1 <netherwarths@gmail.com>
2026-02-26 20:37:45 +0800

    Revert middle name to original label

--- src/main/java/Studentform.java
+++ src/main/java/Studentform.java
@@ -71,7 +71,7 @@ public class Studentform extends javax.swing.JFrame {
             }
         });
 
-        jLabel3.setText("Middle");
+        jLabel3.setText("Middle Name");
 
         Middle_name.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
